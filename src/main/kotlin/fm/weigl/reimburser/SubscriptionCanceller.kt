2e2abd538b16ee1d5d1911f6124d10e84193896b
alec-circle <amreyer@meetcircle.com>
2020-11-25 13:22:02 -0800

    make cancellations asynchronous

--- src/main/kotlin/fm/weigl/reimburser/SubscriptionCanceller.kt
+++ src/main/kotlin/fm/weigl/reimburser/SubscriptionCanceller.kt
@@ -1,5 +1,9 @@
 package fm.weigl.reimburser
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 /**
  * Created by fweigl on 01.11.16.
  */
@@ -22,27 +26,38 @@ class SubscriptionCanceller(publisherProvider: PublisherProvider,
         var totalCount = 0
         val start = System.currentTimeMillis()
 
-        customerData.forEachIndexed { index, customerData ->
+        customerData.forEachIndexed { index, customer ->
 
-            logger.log(TAG, "cancelling abo for customerData $customerData")
-            ++totalCount
-            try {
-                apiService.purchases().subscriptions().cancel(Values.PACKAGE_NAME,
-                    customerData.subscriptionId, customerData.token).execute()
-                logger.log(TAG, "$index,OK,${customerData.toCsv()}")
-                ++successCount
-            } catch (e: Exception) {
-                logger.log("ERROR", "Error cancelling customer $customerData at index $index, $e")
-                logger.log(TAG, "$index,ERROR,${customerData.toCsv()}", true)
-                ++failCount
+            GlobalScope.launch(Dispatchers.IO) {
+                logger.log(TAG, "cancelling abo for customerData $customer")
+                ++totalCount
+                try {
+                    apiService.purchases().subscriptions().cancel(Values.PACKAGE_NAME,
+                        customer.subscriptionId, customer.token).execute()
+                    logger.log(TAG, "$index,OK,${customer.toCsv()}", true)
+                    ++successCount
+                } catch (e: Exception) {
+                    logger.log("ERROR", "Error cancelling customer $customer at index $index, $e")
+                    logger.log(TAG, "$index,ERROR,${customer.toCsv()}", true)
+                    ++failCount
+                }
+
+                if (index == customerData.lastIndex) {
+                    val end = System.currentTimeMillis()
+                    val deltaSeconds = ((end - start) / 1000f)
+
+                    // sleep 15 seconds to allow for trailing responses
+                    Thread.sleep(15000)
+
+                    logger.log("COMPLETE", "Seconds:$deltaSeconds,Total:$totalCount," +
+                        "Success:$successCount,Fail:$failCount", true)
+                }
             }
         }
 
-        val end = System.currentTimeMillis()
-        val deltaSeconds = (end - start) / 1000f
-
-        logger.log("COMPLETE", "Seconds:$deltaSeconds,Total:$totalCount," +
-            "Success:$successCount,Fail:$failCount", true)
+        while (true) {
+            // wait for results to come in
+        }
     }
 
     private fun getCustomerDataSet(): List<CustomerData> {
